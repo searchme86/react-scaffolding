4559d60adf7843767fae77a95bc644ad870960ce
searchme86 <searchme86@gmail.com>
2022-01-17 00:12:18 +0900

    💄 styled button components added & tested

---
+++ src/components/common/Button.js
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+const Button = styled.button`
+  color: white;
+  background: #f8049c;
+  font-weight: bold;
+  border-radius: 4px;
+  box-shadow: none;
+  font-size: 1em;
+  border: none;
+  width: 100%;
+  display: block;
+  white-space: none;
+
+  &:disabled {
+    background: #eee;
+    color: #666;
+  }
+`;
+
+export default Button;
