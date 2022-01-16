0c7f1e7539d29f6cd9f7ad0bca8256c1d5a9408e
searchme86 <searchme86@gmail.com>
2022-01-17 00:11:19 +0900

    🐛 React Absolute path config function added

--- src/App.js
+++
@@ -1,7 +0,0 @@
-import StyledExam from "./components/styled/StyledExam";
-
-function App() {
-  return <StyledExam />;
-}
-
-export default App;
---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import Button from "components/common/Button";
+
+function App() {
+  return (
+    <>
+      <h1>hello world</h1>
+      <div>
+        <Button>Test</Button>
+      </div>
+    </>
+  );
+}
+
+export default App;
