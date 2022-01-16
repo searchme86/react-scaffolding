673141f2816b0b798c971136d4c1d634647f9d51
searchme86 <searchme86@gmail.com>
2022-01-16 22:34:05 +0900

    💄 testing a styled components

--- src/components/styled/StyledExam.js
+++ src/components/styled/StyledExam.js
@@ -12,10 +12,39 @@ const Wrapper = styled.section`
   background-color: papayawhip;
 `;
 
+const Button = styled.button`
+  color: palevioletred;
+  font-size: 1em;
+  margin: 1em;
+  padding: 0.25em 1em;
+  border: 2px solid palevioletred;
+  border-radius: 3px;
+`;
+
+const TomatoButton = styled(Button)`
+  color: tomato;
+  border-radius: tomato;
+`;
+
+const ReversedButton = (props) => (
+  <Button {...props} children={props.children.split("").reverse()} />
+);
+
 function StyeldExam() {
   return (
     <Wrapper>
       <Title>Hello World</Title>
+      <Button>Normal</Button>
+      <Button>primary</Button>
+      <TomatoButton>Tomato</TomatoButton>
+      <br />
+      <hr />
+      <Button as="a" href="{javascript:void(0);}">
+        Link with Button styles
+      </Button>
+      <Button as={ReversedButton}>
+        Custom Button with Normal Button Styles
+      </Button>
     </Wrapper>
   );
 }
