dabf31075dc186e2b807ad7a1759f61460cb61f0
searchme86 <searchme86@gmail.com>
2022-01-17 00:28:01 +0900

    ⚡️ changing ways of importing components

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,20 @@
-import Button from "components/common/Button";
+import { Button } from "components/common";
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  body{
+    background:#fff;
+    min-height: 100vh;
+    margin:0;
+    color:block;
+    font-family: sans-serif;
+  }
+`;
 
 function App() {
   return (
     <>
+      <GlobalStyle />
       <h1>hello world</h1>
       <div>
         <Button>Test</Button>
