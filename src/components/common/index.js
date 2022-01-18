f52562553dea22db06e36b34d4333f1ecdc677da
searchme86 <searchme86@gmail.com>
2022-01-18 15:25:39 +0900

    🔇 🚨 🔥 file deleted

--- src/components/common/index.js
+++
@@ -1,3 +0,0 @@
-export * from "./Button";
-export * from "./PageLayout";
-export * from "./Header";
--- src/components/pages/Login.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { PageLayout } from "components/common";
-
-export default function Login() {
-  return <PageLayout>로그인 페이지 입니다</PageLayout>;
-}
