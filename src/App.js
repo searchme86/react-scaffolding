d46573e8cbe5c3825a2234d333978b2e12bf213c
searchme86 <searchme86@gmail.com>
2022-02-08 15:35:21 +0900

    React App structure empty initialised

--- src/App.js
+++ src/App.js
@@ -1,35 +1,11 @@
-import { Route, Routes } from "react-router-dom";
-import { createGlobalStyle } from "styled-components";
-import Customer from "components/content/Customer/Customer";
-import Faq from "components/content/Faq/Faq";
-import Home from "components/content/Home/Home";
-import Login from "components/content/Login/Login";
-import Layout from "components/Layout";
+import React from 'react';
 
-const GlobalStyle = createGlobalStyle`
-  body{
-    margin:0;
-    padding:0;
-  }
-  a{
-    text-decoration: none;
-  }
-`;
-
-function App() {
-  return (
-    <>
-      <GlobalStyle />
-      <Routes>
-        <Route element={<Layout />}>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/customer" element={<Customer />} />
-          <Route path="/faq" element={<Faq />} />
-        </Route>
-      </Routes>
-    </>
-  );
+function App(){
+  return(
+    <div>
+      <h1>Hello React</h1>
+    </div>
+  )
 }
 
 export default App;
